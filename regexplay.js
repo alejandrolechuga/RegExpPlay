986717bf7452d003d5690b67dac871f0e87ee021
alejandrolechuga <ing.ramon.alejandro@gmail.com>
2014-03-10 23:15:47 -0700

    first commit

---
+++ regexplay.js
@@ -0,0 +1,12 @@
+;(function(window, document, undefined){
+  var RegExpPlay = function () {
+
+  };
+
+  RegExpPlay.prototype = {
+    "match": function () {
+      
+    }
+  };
+  window.RegExpPlay = new RegExpPlay();
+}(this, document));
